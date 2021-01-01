0c4948967cb123216ad1b37dee48292d2823e36b
Johann Boschmann <johannboschmann@hotmail.de>
2021-01-01 13:21:27 +0100

    Finishes client selection implementation.

--- mlmi/selectors.py
+++ mlmi/selectors.py
@@ -1,20 +1,18 @@
-import random
+import numpy as np
 from typing import List
 
-from mlmi.participant import BaseParticipant
+from mlmi.participant import BaseTrainingParticipant
 
 
-class BaseSelector(object):
+def sample_randomly_by_fraction(participants: List['BaseTrainingParticipant'],
+                                fraction: float) -> List['BaseTrainingParticipant']:
+    assert 0.0 <= fraction <= 1.0, 'Client fraction (cf) has to fulfill 0.0 <= cf <= 1.0'
 
-    def select_participants(self, participants: List['BaseParticipant']):
-        raise NotImplementedError()
-
-
-class RandomParticipantFractionSelector(BaseSelector):
-
-    def __init__(self, fraction: float = 1.0):
-        self._fraction = fraction
-
-    def select_participants(self, participants: List['BaseParticipant']):
-        target_num = int(self._fraction * len(participants)) + 1
-        indices = [random.randint()]
+    out = []
+    num_participants = len(participants)
+    num_out = int(fraction * num_participants) if fraction != 0.0 else 1
+    indices = np.arange(num_participants)
+    random_indices = np.random.choice(indices, size=num_out, replace=False)
+    for i in random_indices:
+        out.append(participants[i])
+    return out
