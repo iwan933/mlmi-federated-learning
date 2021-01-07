d71eb1029788e2f8fc160b2de2b39da0095a99b7
Maximilian <maximilian.frantzen@inveox.com>
2021-01-07 10:26:18 +0100

    using Ward linkage mechanism as in Briggs paper

--- mlmi/fedavg/run.py
+++ mlmi/fedavg/run.py
@@ -63,7 +63,7 @@ def run_fedavg_hierarchical(context: ExperimentContext, num_rounds_init: int, nu
     learning_rate = 0.03
     optimizer_args = OptimizerArgs(optim.SGD, lr=learning_rate)
     model_args = ModelArgs(CNNLightning, optimizer_args, only_digits=False)
-    cluster_args = ClusterArgs(linkage_mech='single', dis_metric='euclidean', criterion='maxclust', max_value_criterion=4)
+    cluster_args = ClusterArgs(linkage_mech='ward', dis_metric='euclidean', criterion='maxclust', max_value_criterion=4)
     if torch.cuda.is_available():
         training_args = TrainArgs(max_steps=steps, gpus=1)
     else:
