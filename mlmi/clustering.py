e2c6fcd0e75077ea177dd5c2c6abbd9fd8c37d00
Maximilian <maximilian.frantzen@inveox.com>
2021-01-09 19:19:26 +0100

    finished function for computing mean of weights over all layers for clustering of clients

--- mlmi/clustering.py
+++ mlmi/clustering.py
@@ -41,34 +41,39 @@ class GradientClusterPartitioner(BaseClusterPartitioner):
         self.dis_metric = cluster_args.dis_metric
         self.max_value_criterion = cluster_args.max_value_criterion
 
+
+    def model_weigths(self, participant: BaseParticipant):
+        key_layers_participant = list(participant.get_model().state_dict().keys())
+        num_layers = int(len(participant.get_model().state_dict().keys()) / 2)
+        accumulated_weights_participant = 0
+        for layer in range(num_layers):
+            layer_dim = participant.get_model().state_dict()[key_layers_participant[layer*2]].squeeze().dim()
+            weights_layer = participant.get_model().state_dict()[key_layers_participant[layer * 2]].squeeze()
+            mean_weights_layer = weights_layer.mean(tuple(range(layer_dim)))
+            mean_weights_layer = float(mean_weights_layer)
+            accumulated_weights_participant = accumulated_weights_participant + mean_weights_layer
+
+        return accumulated_weights_participant
+
     def cluster(self, participants: List[BaseParticipant]) -> Dict[str, List[BaseParticipant]]:
         clusters_hac_dic = {}
-
-        def model_weigths(participant):
-            key_layers_participant = list(participant.get_model().state_dict().keys())
-            num_layers = int(len(participant.get_model().state_dict().keys()) / 2)
-            # ToDo: Mean over all dimensions
-            accumulated_weights_participant = 0
-            for layer in range(num_layers):
-                mean_weight_layer = participant.get_model().state_dict()[key_layers_participant[layer*2]].squeeze().mean((0,1,2))
-                print(mean_weight_layer.size())
-                mean_weight_layer = float(mean_weight_layer)
-                accumulated_weights_participant = accumulated_weights_participant + mean_weight_layer
-            return accumulated_weights_participant
 
         # Compute distance matrix of model updates: Using mean of weights from last layer of each participant
         model_updates = np.array([])
         for participant in participants:
-            accumulated_weights_participant = model_weigths(participant)
-            print(accumulated_weights_participant)
+            accumulated_weights_participant = self.model_weigths(participant)
+            model_updates = np.append(model_updates, accumulated_weights_participant)
+
+            """"
             weights_last_layer_key = list(participant.get_model().state_dict().keys())[-2]
             weights_last_layer = participant.get_model().state_dict()[weights_last_layer_key]
             model_updates = np.append(model_updates, mean(weights_last_layer).numpy())
+            """
         model_updates = np.reshape(model_updates, (len(model_updates), 1))
         distance_matrix = hac.linkage(model_updates, method=self.linkage_mech, metric=self.dis_metric, optimal_ordering=False)
 
         # Alternative
-        #cluster_ids_alt = hac.fclusterdata(model_updates, 4, criterion="maxclust", metric="euclidean", method="single")
+        # cluster_ids_alt = hac.fclusterdata(model_updates, 4, criterion="maxclust", metric="euclidean", method="single")
 
         # Compute clusters based on distance matrix
         cluster_ids = hac.fcluster(distance_matrix, self.max_value_criterion, self.criterion)
