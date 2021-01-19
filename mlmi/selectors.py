75a4dda4d7346dbc90c629521c33a90c9c7ef36b
Johann Boschmann <johannboschmann@hotmail.de>
2021-01-19 21:58:56 +0100

    Forces client sampling always to return at least one client.

--- mlmi/selectors.py
+++ mlmi/selectors.py
@@ -10,7 +10,7 @@ def sample_randomly_by_fraction(participants: List['BaseTrainingParticipant'],
 
     out = []
     num_participants = len(participants)
-    num_out = int(fraction * num_participants) if fraction != 0.0 else 1
+    num_out = int(fraction * num_participants) if int(fraction * num_participants) > 1 else 1
     indices = np.arange(num_participants)
     random_indices = np.random.choice(indices, size=num_out, replace=False)
     for i in random_indices:
