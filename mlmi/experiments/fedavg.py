bf1558b2b23103aef1dfd21b79c70e499ffc2314
Johann Boschmann <johannboschmann@hotmail.de>
2021-02-03 07:34:46 +0100

    Adds vanilla fedavg experiment for mnist and emnist.

---
+++ mlmi/experiments/fedavg.py
@@ -0,0 +1,119 @@
+from typing import Callable, Dict, List, Optional
+
+from sacred import Experiment
+from functools import partial
+
+from torch import Tensor, optim
+
+from mlmi.clustering import ModelFlattenWeightsPartitioner
+from mlmi.experiments.log import log_goal_test_acc, log_loss_and_acc
+from mlmi.fedavg.femnist import load_femnist_dataset, load_mnist_dataset
+from mlmi.fedavg.model import CNNLightning, CNNMnistLightning, FedAvgServer
+from mlmi.fedavg.run import run_fedavg
+from mlmi.fedavg.structs import FedAvgExperimentContext
+from mlmi.fedavg.util import load_fedavg_state, run_fedavg_round, run_fedavg_train_round
+from mlmi.hierarchical.run import run_fedavg_hierarchical
+from mlmi.participant import BaseTrainingParticipant
+from mlmi.plot import generate_client_label_heatmap, generate_data_label_heatmap
+from mlmi.settings import REPO_ROOT
+from mlmi.structs import ClusterArgs, FederatedDatasetData, ModelArgs, OptimizerArgs, TrainArgs
+from mlmi.utils import create_tensorboard_logger, fix_random_seeds, overwrite_participants_models
+
+ex = Experiment('hierachical_clustering')
+
+
+@ex.config
+def femnist():
+    seed = 123123123
+    lr = 0.1
+    name = 'femnist'
+    total_fedavg_rounds = 50
+    client_fraction = [0.1]
+    local_epochs = 3
+    batch_size = 10
+    num_clients = 367
+    num_classes = 62
+    optimizer_args = OptimizerArgs(optim.SGD, lr=lr)
+    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
+    model_args = ModelArgs(CNNLightning, optimizer_args=optimizer_args, only_digits=False)
+    dataset = 'femnist'
+
+
+@ex.named_config
+def mnist():
+    seed = 123123123
+    lr = 0.1
+    name = 'mnist'
+    total_fedavg_rounds = 50
+    client_fraction = [0.1]
+    local_epochs = 3
+    batch_size = 10
+    num_clients = 100
+    num_classes = 10
+    optimizer_args = OptimizerArgs(optim.SGD, lr=lr)
+    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
+    model_args = ModelArgs(CNNMnistLightning, optimizer_args=optimizer_args, num_classes=num_classes)
+    dataset = 'mnist'
+
+
+def log_after_round_evaluation(
+        experiment_logger,
+        tag: str,
+        loss: Tensor,
+        acc: Tensor,
+        step: int
+):
+    log_loss_and_acc(tag, loss, acc, experiment_logger, step)
+    log_goal_test_acc(tag, acc, experiment_logger, step)
+
+
+def log_dataset_distribution(experiment_logger, tag: str, dataset: FederatedDatasetData):
+    dataloaders = list(dataset.train_data_local_dict.values())
+    image = generate_data_label_heatmap(tag, dataloaders, dataset.class_num)
+    experiment_logger.experiment.add_image('label distribution', image.numpy())
+
+
+@ex.automain
+def run_hierarchical_clustering(
+        seed,
+        lr,
+        name,
+        total_fedavg_rounds,
+        client_fraction,
+        local_epochs,
+        batch_size,
+        num_clients,
+        optimizer_args,
+        train_args,
+        model_args,
+        dataset,
+):
+    fix_random_seeds(seed)
+
+    if dataset == 'femnist':
+        fed_dataset = load_femnist_dataset(str((REPO_ROOT / 'data').absolute()),
+                                           num_clients=num_clients, batch_size=batch_size)
+    elif dataset == 'mnist':
+        fed_dataset = load_mnist_dataset(str((REPO_ROOT / 'data').absolute()),
+                                         num_clients=num_clients, batch_size=batch_size)
+    else:
+        raise ValueError(f'dataset "{dataset}" unknown')
+
+    data_distribution_logged = False
+    for cf in client_fraction:
+        fedavg_context = FedAvgExperimentContext(name=name, client_fraction=cf, local_epochs=local_epochs,
+                                                 lr=lr, batch_size=batch_size, optimizer_args=optimizer_args,
+                                                 model_args=model_args, train_args=train_args,
+                                                 dataset_name=dataset)
+        experiment_specification = f'{fedavg_context}'
+        experiment_logger = create_tensorboard_logger(fedavg_context.name, experiment_specification)
+
+        if not data_distribution_logged:
+            log_dataset_distribution(experiment_logger, 'full dataset', fed_dataset)
+            data_distribution_logged = True
+
+        log_after_round_evaluation_fns = [
+            partial(log_after_round_evaluation, experiment_logger, 'fedavg')
+        ]
+        run_fedavg(context=fedavg_context, num_rounds=total_fedavg_rounds, dataset=fed_dataset,
+                   save_states=True, restore_state=True, after_round_evaluation=log_after_round_evaluation_fns)
--- mlmi/fedavg/model.py
+++ mlmi/fedavg/model.py
@@ -121,13 +121,14 @@ class CNNMnist(nn.Module):
         self.conv1 = nn.Conv2d(input_channels, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
+        self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, num_classes)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = torch.flatten(x)
+        x = self.flatten(x)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
