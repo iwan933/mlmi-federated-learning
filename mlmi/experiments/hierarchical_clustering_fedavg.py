788d573222903eb77ba816b2adc0b0b6f7ef4184
Maximilian <maximilian.frantzen@inveox.com>
2021-02-14 10:51:30 +0100

    configs for AWS runs

--- mlmi/experiments/hierarchical_clustering_fedavg.py
+++ mlmi/experiments/hierarchical_clustering_fedavg.py
@@ -54,7 +54,7 @@ def default_configuration():
 @ex.named_config
 def fedavg_hierachCluster_color():
     seed = 123123123
-    lr = [0.069]
+    lr = [0.08]
     name = 'color_hpsearch_lr'
     total_fedavg_rounds = 50
     cluster_initialization_rounds = [1]
