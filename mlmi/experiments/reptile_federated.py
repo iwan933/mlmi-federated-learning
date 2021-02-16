ee4a460be9ee292dd6d6a38266a1ccf0ad737dad
richardgaus <richardgaus@outlook.de>
2021-02-16 18:13:38 +0100

    Changes Reptile parameters

--- mlmi/experiments/reptile_federated.py
+++ mlmi/experiments/reptile_federated.py
@@ -41,7 +41,7 @@ def femnist():
     num_clients_test = 0  # Used only with dataset='omniglot'
     meta_batch_size = 5
     num_meta_steps = 20000
-    meta_learning_rate_initial = 0.1
+    meta_learning_rate_initial = 1
     meta_learning_rate_final = 0
 
     eval_interval = 250
@@ -50,7 +50,7 @@ def femnist():
     num_eval_clients_final = -1
 
     inner_batch_size = 10
-    inner_learning_rate = [0.005]
+    inner_learning_rate = [0.03]
     num_inner_steps = 5
     num_inner_steps_eval = 50
 
