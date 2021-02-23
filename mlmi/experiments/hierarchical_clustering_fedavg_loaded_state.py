ff37706b16eb6cd70fb6b83803d94ed5b93314b1
Johann Boschmann <johannboschmann@hotmail.de>
2021-02-23 17:32:56 +0100

    Add hierarchical for loaded state.

--- mlmi/experiments/hierarchical_clustering_fedavg_loaded_state.py
+++ mlmi/experiments/hierarchical_clustering_fedavg_loaded_state.py
@@ -32,89 +32,6 @@ logger = getLogger(__name__)
 ex = Experiment('hierachical_clustering')
 
 
-@ex.config
-def default_configuration():
-    local_evaluation_steps = 1
-    seed = 123123123
-    lr = 0.068
-    name = 'default_hierarchical_fedavg'
-    total_fedavg_rounds = 10
-    cluster_initialization_rounds = [3, 5, 10]
-    client_fraction = [0.1]
-    local_epochs = 1
-    batch_size = 10
-    num_clients = 367
-    sample_threshold = -1
-    num_label_limit = -1
-    num_classes = 62
-    use_colored_images = True
-    use_pattern = True
-    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    train_cluster_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    dataset = 'femnist'
-    partitioner_class = AlternativePartitioner
-    linkage_mech = 'ward'
-    criterion = 'distance'
-    dis_metric = 'euclidean'
-    max_value_criterion = 6.5
-    reallocate_clients = False
-    threshold_min_client_cluster = 80
-
-
-@ex.named_config
-def fedavg_hierachCluster_color():
-    seed = 123123123
-    lr = [0.065]
-    name = 'color_hpsearch_scratch'
-    total_fedavg_rounds = 75
-    cluster_initialization_rounds = [5, 10, 15, 20]
-    client_fraction = [0.1]
-    local_epochs = 3
-    batch_size = 10
-    num_clients = 367
-    sample_threshold = 250  # we need clients with at least 250 samples to make sure all labels are present
-    num_label_limit = 20
-    num_classes = 62
-    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    dataset = 'femnist'
-    partitioner_class = ModelFlattenWeightsPartitioner
-    linkage_mech = 'ward'
-    criterion = 'distance'
-    dis_metric = 'euclidean'
-    max_value_criterion = [6, 8, 10, 12, 14]
-    reallocate_clients = False
-    threshold_min_client_cluster = 40
-    use_colored_images = True
-
-
-@ex.named_config
-def color_and_pattern():
-    local_evaluation_steps = 14
-    seed = 123123123
-    lr = [6.50E-02]
-    name = 'color_and_pattern'
-    total_fedavg_rounds = 75
-    cluster_initialization_rounds = [8]
-    client_fraction = [0.1]
-    local_epochs = 3
-    batch_size = 10
-    num_clients = 367
-    sample_threshold = -1  # we need clients with at least 250 samples to make sure all labels are present
-    num_label_limit = -1
-    num_classes = 62
-    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    dataset = 'femnist'
-    partitioner_class = ModelFlattenWeightsPartitioner
-    linkage_mech = 'ward'
-    criterion = 'distance'
-    dis_metric = 'euclidean'
-    max_value_criterion = [6.00]
-    reallocate_clients = False
-    threshold_min_client_cluster = 40
-    use_colored_images = True
-    use_pattern = True
-
-
 @ex.named_config
 def ham10k():
     local_evaluation_steps = 7
@@ -144,83 +61,6 @@ def ham10k():
     use_pattern = False
     mean = (0.485, 0.456, 0.406)
     std = (0.229, 0.224, 0.225)
-
-
-@ex.named_config
-def hpsearch():
-    seed = 123123123
-    lr = [0.1]
-    name = 'briggs_Clust0'
-    total_fedavg_rounds = 15
-    cluster_initialization_rounds = [10]
-    client_fraction = [0.1]
-    local_epochs = 3
-    batch_size = 10
-    num_clients = 367
-    sample_threshold = 250  # we need clients with at least 250 samples to make sure all labels are present
-    num_label_limit = 15
-    num_classes = 62
-    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    dataset = 'femnist'
-    partitioner_class = ModelFlattenWeightsPartitioner
-    linkage_mech = 'ward'
-    criterion = 'distance'
-    dis_metric = 'euclidean'
-    max_value_criterion = [1]
-    reallocate_clients = False
-    threshold_min_client_cluster = 80
-    use_colored_images = False
-
-
-@ex.named_config
-def datadependent_clustering():
-    seed = 123123123
-    lr = [0.1]
-    name = 'briggs_dataClust2'
-    total_fedavg_rounds = 50
-    cluster_initialization_rounds = [1, 3, 5, 10]
-    client_fraction = [0.1]
-    local_epochs = 3
-    batch_size = 10
-    num_clients = 367
-    sample_threshold = -1  # we need clients with at least 250 samples to make sure all labels are present
-    num_label_limit = -1
-    num_classes = 62
-    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    train_cluster_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    dataset = 'femnist'
-    partitioner_class = DatadependentPartitioner
-    linkage_mech = 'ward'
-    criterion = 'distance'
-    dis_metric = 'euclidean'
-    max_value_criterion = [100.0, 150.0]
-    reallocate_clients = False
-    threshold_min_client_cluster = 80
-    use_colored_images = False
-
-
-@ex.named_config
-def briggs():
-    seed = 123123123
-    lr = 0.1
-    name = 'briggs'
-    total_fedavg_rounds = 50
-    cluster_initialization_rounds = [1, 3, 5, 10]
-    client_fraction = [0.1, 0.2, 0.5]
-    local_epochs = 3
-    batch_size = 10
-    num_clients = 367
-    sample_threshold = 250  # we need clients with at least 250 samples to make sure all labels are present
-    num_label_limit = 15
-    num_classes = 62
-    train_args = TrainArgs(max_epochs=local_epochs, min_epochs=local_epochs, progress_bar_refresh_rate=0)
-    dataset = 'femnist'
-    partitioner_class = ModelFlattenWeightsPartitioner
-    linkage_mech = 'ward'
-    criterion = 'distance'
-    dis_metric = 'euclidean'
-    max_value_criterion = 10.0
-    threshold_min_client_cluster = 1
 
 
 def log_after_round_evaluation(
@@ -345,19 +185,7 @@ def run_hierarchical_clustering(
     global_tag = 'global_performance'
     global_tag_local = 'global_performance_personalized'
     initialize_clients_fn = DEFAULT_CLIENT_INIT_FN
-    if dataset == 'femnist':
-        if use_colored_images:
-            fed_dataset = load_femnist_colored_dataset(str((REPO_ROOT / 'data').absolute()),
-                                                       num_clients=num_clients, batch_size=batch_size,
-                                                       sample_threshold=sample_threshold,
-                                                       add_pattern=use_pattern)
-        else:
-            fed_dataset = load_femnist_dataset(str((REPO_ROOT / 'data').absolute()),
-                                               num_clients=num_clients, batch_size=batch_size,
-                                               sample_threshold=sample_threshold)
-        if num_label_limit != -1:
-            fed_dataset = scratch_labels(fed_dataset, num_label_limit)
-    elif dataset == 'ham10k':
+    if dataset == 'ham10k':
         fed_dataset = load_ham10k_federated(partitions=num_clients, batch_size=batch_size, mean=mean, std=std)
         initialize_clients_fn = initialize_ham10k_clients
     else:
@@ -367,15 +195,11 @@ def run_hierarchical_clustering(
         max_value_criterion = [max_value_criterion]
     if not hasattr(lr, '__iter__'):
         lr = [lr]
-    input_channels = 3 if use_colored_images else 1
-    data_distribution_logged = False
+
     for cf in client_fraction:
         for lr_i in lr:
             optimizer_args = OptimizerArgs(optim.SGD, lr=lr_i)
-            model_args = ModelArgs(CNNLightning, optimizer_args=optimizer_args,
-                                   input_channels=input_channels, only_digits=False)
-            if dataset == 'ham10k':
-                model_args = ModelArgs(MobileNetV2Lightning, optimizer_args=optimizer_args, num_classes=7)
+            model_args = ModelArgs(MobileNetV2Lightning, optimizer_args=optimizer_args, num_classes=7)
             fedavg_context = FedAvgExperimentContext(name=name, client_fraction=cf, local_epochs=local_epochs,
                                                      lr=lr_i, batch_size=batch_size, optimizer_args=optimizer_args,
                                                      model_args=model_args, train_args=train_args,
@@ -383,27 +207,15 @@ def run_hierarchical_clustering(
             experiment_specification = f'{fedavg_context}'
             experiment_logger = create_tensorboard_logger(fedavg_context.name, experiment_specification)
             fedavg_context.experiment_logger = experiment_logger
-            if not data_distribution_logged:
-                log_dataset_distribution(experiment_logger, 'full dataset', fed_dataset)
-                data_distribution_logged = True
-
-            log_after_round_evaluation_fns = [
-                partial(log_after_round_evaluation, experiment_logger, 'fedavg'),
-                partial(log_after_round_evaluation, experiment_logger, global_tag)
-            ]
-            log_after_aggregation = [
-                partial(log_personalized_performance, experiment_logger, 'fedavg_personalized', local_evaluation_steps),
-                partial(log_personalized_performance, experiment_logger, global_tag_local, local_evaluation_steps)
-            ]
-            server, clients = run_fedavg(context=fedavg_context, num_rounds=total_fedavg_rounds, dataset=fed_dataset,
-                                         save_states=True, restore_state=True,
-                                         after_round_evaluation=log_after_round_evaluation_fns,
-                                         after_aggregation=log_after_aggregation,
-                                         initialize_clients_fn=initialize_clients_fn)
-
             for init_rounds, max_value in generate_configuration(cluster_initialization_rounds, max_value_criterion):
                 # load the model state
                 round_model_state = load_fedavg_state(fedavg_context, init_rounds)
+
+                server = FedAvgServer('initial_server', fedavg_context.model_args, fedavg_context)
+                server.overwrite_model_state(round_model_state)
+                logger.info('initializing clients ...')
+                clients = initialize_clients_fn(fedavg_context, fed_dataset, server.model.state_dict())
+
                 overwrite_participants_models(round_model_state, clients)
                 # initialize the cluster configuration
                 round_configuration = {
